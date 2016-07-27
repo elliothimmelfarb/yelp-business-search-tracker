cf8704a6d98a02b437ea65cebfdf39dd05fad695
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-07-26 22:56:06 -0700

    removed facebook

--- client/js/config/auth.config.js
+++ client/js/config/auth.config.js
@@ -7,10 +7,5 @@
     const authP = $authProvider;
     authP.loginUrl = '/api/users/login';
     authP.signupUrl = '/api/users/register';
-
-    authP.facebook({
-      clientId: '282619428795743',
-      url: '/api/users/facebook',
-    });
   }
 })();
